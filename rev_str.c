1c88ac75f0a3f0532d3099fdf17e6a2c9a91ce86
vijaysathvikab <vijaysathvikab@gmail.com>
2019-09-27 19:17:23 +0530

    tried to fix Bugs and other improvments

--- rev_str.c
+++ rev_str.c
@@ -1,38 +1,58 @@
-
 //Program to reverse a string | Vijay Sathvika B | 29 aug 2019
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-char *OriginalString, *ReverseString;
-char Reverse_String(char OriginalString[]);
+char *Reverse_String(char *OriginalString, char revstr[], int size);
+
 
 void main()
 {   
-	int size;
-	//Input size
-	printf("Enter the size of the string: ");
-	scanf("%d",&size);
-	OriginalString=(char*)malloc(size*sizeof(char));
-	ReverseString=(char*)malloc(size*sizeof(char));
+	char *ptr;
+	int i,l;
+	char *OriginalString1, *OriginalString2;
+	
+
+	OriginalString1=(char*)malloc(100*sizeof(char));
+	
+
 	//Input string
     printf("Enter the string: ");
-    scanf("%s", OriginalString);
-    Reverse_String(OriginalString);
-    printf("The reverse string is:%s", ReverseString);
-    free(OriginalString);
-    free(ReverseString);
+    gets(OriginalString1);
+    //Allocates memory
+    l=strlen(OriginalString1);
+    printf("%d", l);
+    
+    OriginalString2=(char*)malloc((l+2)*sizeof(char));
+    strcpy(OriginalString2, OriginalString1);
+    free(OriginalString1);
+    
+    
+	char ReverseString[sizeof(OriginalString2)];
+	
+	Reverse_String(OriginalString2, ReverseString, l);
+    printf("\nThe reverse string is: %s \n", ReverseString);
 }
 
 //Function to reverse the string
-char Reverse_String(char *OriginalString)
+char *Reverse_String(char *OriginalString, char ReverseString[], int size)
 {
-	int i,n;
-    n=strlen(OriginalString);
-    for(i=0; *(OriginalString+i) != '\0'; i++,n--)
+	char *ptr;
+	int i;
+	char reversestring[sizeof(OriginalString)];
+    ptr=reversestring;
+    for(i=0; i<size ; i++)
     {
-    	*(ReverseString+n) = *(OriginalString+i);
+    	*ptr = *(OriginalString+i);
+    	ptr++;
     }
-    *(ReverseString+i+1)='\0';
-	return *ReverseString;
-}
+    free(OriginalString);
+    for(i=0;ptr!=reversestring-1; i++)
+    {
+    	ReverseString[i]=*(ptr-1);
+    	ptr--;
+	}
+    ReverseString[i+1]='\0';
+	return ReverseString;
+}       
+
